8c4eda76c1e9fceeb21b26734b2e60203524b72e
randaline11 <astarr2015@gmail.com>
2016-12-01 14:29:32 -0500

    some basic state added and possible parse interaction?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+ import Parse from 'parse';
+ //var Parse = require('parse');
+// global.Parse = require('parse-1.6.14').Parse;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.clickedSomething =  this.clickedSomething.bind(this);
+    this.checkLoginForm =  this.checkLoginForm.bind(this);
+    this.handleChangeEmail =  this.handleChangeEmail.bind(this);
+    this.handleChangePassword =  this.handleChangePassword.bind(this);
+  }
+
+clickedSomething(event) {
+   alert(`the value of email is:${this.state.email}`);
+   event.preventDefault();
+  }
+
+handleChangeEmail(event) {
+  this.setState({email: event.target.value});
+}
+
+handleChangePassword(event) {
+  this.setState({password: event.target.value});
+}
+
+  validateLogin(event) {
+    alert(`the email is: ${this.state.email}`);
+    alert(`the pwis: ${this.state.password}`);
+
+
+  if( this.state.email == "" )
+  {
+    alert("Please enter email address.");
+  //  $("#si_email").focus();
+    return false;
+  }
+
+  /*
+  if( this.state.email.indexOf('@dartmouth.edu') == -1 ){
+    alert("Please enter a valid dartmouth email id.");
+  //  $("#si_email").focus();
+    return false;
+  }
+  */
+
+  if( this.state.password == "" )
+  {
+    alert("Please enter password.");
+  //  $("#si_password").focus();
+    return false;
+  }
+
+
+  return true;
+
+  } //validateLogin
+
+checkLoginForm(){
+if (this.validateLogin()==false){
+return false;
+}else{
+
+var username=this.state.email;
+var password=this.state.password;
+
+if(1==1) {
+  return true;
+
+
+
+
+  Parse.User.logIn(username, password, {
+success: function(user) {
+  // Do stuff after successful login.
+// window.location = "profile.php";
+ alert('Lookee here you logged in!');
+return true;
+},
+error: function(user, error) {
+  // The login failed. Check error to see why.
+alert("Error: User login failed. Please check your username and password.");
+return false;
+}
+});
+}else{
+alert("Sorry! Site is currently undergoing development. We'll be back up soon!");
+}
+
+}
+return false;
+}
+
+
   render() {
     return (
       <div className="App">
@@ -13,16 +109,16 @@ class App extends Component {
 
       <div className="login_box">
         <div className="title">Login to your BookUp account</div>
-          <form>
+          <form onSubmit={this.checkLoginForm}>
             <label>
               <div className="form-group">
-                <input type="text" className="form-control" name="email" placeholder="Enter email" />
+                <input type="text" className="form-control" name="email" placeholder="Enter email" onChange={this.handleChangeEmail}/>
               </div>
             </label>
 
             <label>
               <div className="form-group">
-                <input type="password" className="form-control" name="password" placeholder="Enter password"/>
+                <input type="password" className="form-control" name="password" placeholder="Enter password" onChange={this.handleChangePassword}/>
               </div>
             </label>
             <div className="submit">
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Parse from 'parse';
+import Parse_React from 'parse-react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
